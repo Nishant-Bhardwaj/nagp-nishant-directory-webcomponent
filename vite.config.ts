87db6f94e89c5c24d36fa1f0dcb979d8e0d71d3f
Nishant Bhardwaj <52878003+Nishant-Bhardwaj@users.noreply.github.com>
2023-05-13 19:10:15 +0530

    Final with dist JS file

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import { build, defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  build:{
+    lib:{
+      entry: './src/main.ts',
+      name: 'nagp-nishant-directory-webcomponent'
+    }
+  },
+  plugins: [
+    svelte({
+      compilerOptions: {
+        customElement: true
+      }
+    })
+  ],
+  
 })
